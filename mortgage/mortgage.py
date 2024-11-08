83c8a05970a5f9129261f3c44542b612ac9e6849
Yunfei Wu <ywu16@academic.rrc.ca>
2024-11-08 17:53:04 -0600

    Define Mortgage class with validation for Loan Amount, Rate, Frequency, and Amortization.

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -6,4 +6,46 @@ Usage: Create an instance of the Mortgage class to manage mortgage
 records and calculate payments.
 """
 
+from mortgage.pixell_lookup import MortgageRate, PaymentFrequency, VALID_AMORTIZATION
 
+class Mortgage:
+    """
+    A class to represent a mortgage loan.
+    """
+
+    def __init__(self, loan_amount: float, rate: str, frequency: str, amortization: int):
+        """
+        Initialize a new mortgage with the provided parameters.
+
+        Args:
+        Loan_amount (float): The amount of the mortgage loan.
+        Rate (str): The annual interest rate as a string (e.g., "FIXED_5").
+        Frequency (str): The payment frequency as a string (e.g., "MONTHLY").
+        Amortization (int): The number of years for the mortgage loan repayment.
+
+        Raises:
+        ValueError: Raise when any of the provided values are invalid.
+        """
+
+        # Loan Amount Validation
+        if loan_amount <= 0:
+            raise ValueError("Loan Amount must be positive.")
+        self.__loan_amount = loan_amount
+
+        # Rate Validation
+        try:
+            self.__rate = MortgageRate[rate]
+        except Exception as e:
+            raise ValueError("Rate provided is invalid.")
+
+        # Frequency Validation
+        try:
+            self.__frequency = PaymentFrequency[frequency]
+        except Exception as e:
+            raise ValueError("Frequency provided is invalid.")
+
+        # Amortization Validation
+        if amortization not in VALID_AMORTIZATION:
+            raise ValueError("Amortization provided is invalid.")
+        self.__amortization = amortization
+   
